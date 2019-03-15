6e08e49331b525043754f37e0d4a668229f4b71d
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-03-15 11:59:12 -0300

    Prettify and add current date

--- src/components/Moon/Info.js
+++ src/components/Moon/Info.js
@@ -1,29 +1,32 @@
-import React from "react"
-import MoonPhase from "."
-import { Flex } from "rebass"
-import Text from "../Text"
-import { format, addDays } from "date-fns"
-import MoonCircle from "./Circle"
+import React from 'react';
+import MoonPhase from '.';
+import { Flex } from 'rebass';
+import Text from '../Text';
+import { format, addDays } from 'date-fns';
+import MoonCircle from './Circle';
 
 const MoonInfo = () => {
   return (
     <MoonPhase>
       {({ moon, hunt }) => {
-        const nextEkadasi = addDays(hunt().new_date, 11)
+        const nextEkadasi = addDays(hunt().new_date, 11);
         return (
           <Flex flexDirection="column" alignItems="center">
-            <MoonCircle size={240} moon={moon()} mb={[4, 5]} />
+            <MoonCircle size={240} moon={moon()} mb={4} />
+            <Text fontSize={4} mb={[4, 5]}>
+              {format(new Date(), 'DD MMM YYYY')}
+            </Text>
             <Flex flexDirection="column" alignItems="center">
               <Text fontSize={3} variant="secondary">
                 Next Ekadashi
               </Text>
-              <Text fontSize={4}>{format(nextEkadasi, "DD MMM")}</Text>
+              <Text fontSize={4}>{format(nextEkadasi, 'DD MMM')}</Text>
             </Flex>
           </Flex>
-        )
+        );
       }}
     </MoonPhase>
-  )
-}
+  );
+};
 
-export default MoonInfo
+export default MoonInfo;
