a2fb2321611d11b664970b6d2a6e52ff63561b7e
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-03-16 13:42:34 -0300

    Update MoonInfo
    - Use GeoProvider
    - Use Ekadasi
    - Hoist date
    - Update date formats
    - Set propTypes

--- src/components/Moon/Info.js
+++ src/components/Moon/Info.js
@@ -1,32 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MoonPhase from '.';
 import { Flex } from 'rebass';
-import Text from '../Text';
-import { format, addDays } from 'date-fns';
-import MoonCircle from './Circle';
 
-const MoonInfo = () => {
+import Text from '../Text';
+import { format } from 'date-fns';
+import MoonCircle from './Circle';
+import Ekadasi from '../Ekadasi';
+import Geo from '../Geo';
+
+const MoonInfo = ({ date }) => {
   return (
-    <MoonPhase>
-      {({ moon, hunt }) => {
-        const nextEkadasi = addDays(hunt().new_date, 11);
+    <MoonPhase date={date}>
+      {({ moon }) => {
         return (
           <Flex flexDirection="column" alignItems="center">
-            <MoonCircle size={240} moon={moon()} mb={4} />
+            <MoonCircle size={240} moon={moon(date)} mb={4} />
             <Text fontSize={4} mb={[4, 5]}>
-              {format(new Date(), 'DD MMM YYYY')}
+              {format(date, 'DD MMM YYYY')}
             </Text>
             <Flex flexDirection="column" alignItems="center">
               <Text fontSize={3} variant="secondary">
                 Next Ekadashi
               </Text>
-              <Text fontSize={4}>{format(nextEkadasi, 'DD MMM')}</Text>
+              <Geo>
+                {({ geo }) => (
+                  <Ekadasi date={date} geo={geo}>
+                    {({ nextEkadasi }) => (
+                      <Text fontSize={4}>
+                        {format(nextEkadasi, 'dddd, DD MMM')}
+                      </Text>
+                    )}
+                  </Ekadasi>
+                )}
+              </Geo>
             </Flex>
           </Flex>
         );
       }}
     </MoonPhase>
   );
+};
+
+MoonInfo.propTypes = {
+  date: PropTypes.object,
+};
+
+MoonInfo.defaultProps = {
+  date: new Date(),
 };
 
 export default MoonInfo;
