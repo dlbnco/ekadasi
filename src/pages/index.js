6f65e0687ce4161880c73544a44f79d8b7a2960d
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-03-16 13:43:05 -0300

    Hoist date in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import React from "react"
+import React from 'react';
 
-import MoonInfo from "../components/Moon/Info"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Container from "../components/Container"
+import MoonInfo from '../components/Moon/Info';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Container from '../components/Container';
+
+const date = new Date();
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`ekadasi`, `fasting`]} />
     <Container maxWidth={640}>
-      <MoonInfo />
+      <MoonInfo date={date} />
     </Container>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
