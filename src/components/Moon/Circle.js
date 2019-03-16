abbae3c3e4e069861cdff600cdc7f9d033077518
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-03-16 13:42:58 -0300

    Update MoonCircle

--- src/components/Moon/Circle.js
+++ src/components/Moon/Circle.js
@@ -1,15 +1,17 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Flex, Box } from "rebass"
-import styled, { css } from "styled-components"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Flex, Box } from 'rebass';
+import styled, { css } from 'styled-components';
 
 const Wrapper = styled(Flex)`
-  ${({ size }) => css`
+  position: relative;
+  transition: transform 1s;
+  ${({ size, rotation }) => css`
     width: ${size}px;
     height: ${size}px;
+    transform: rotate(${rotation}rad);
   `}
-  position: relative;
-`
+`;
 
 const Half = styled(Box).attrs({ width: 1 })`
   overflow: hidden;
@@ -19,29 +21,29 @@ const Half = styled(Box).attrs({ width: 1 })`
     css`
       transform: scaleX(-1);
     `}
-`
+`;
 
-const getNormalizedPhase = phase => (phase <= 0.5 ? phase : 1 - phase)
+const getNormalizedPhase = phase => (phase <= 0.5 ? phase : 1 - phase);
 
 const getRotationRad = phase => {
-  const normalizedPhase = getNormalizedPhase(phase)
-  const rad = (Math.PI * normalizedPhase) / 0.5
-  return rad
-}
+  const normalizedPhase = getNormalizedPhase(phase);
+  const rad = (Math.PI * normalizedPhase) / 0.5;
+  return rad;
+};
 
 const Difference = styled.div`
   position: absolute;
   z-index: 1;
   left: 50%;
   background: ${({ phase }) =>
-    0.5 / getNormalizedPhase(phase) > 2 ? "black" : "white"};
+    0.5 / getNormalizedPhase(phase) > 2 ? 'black' : 'white'};
   ${({ size, phase }) => css`
     width: ${size}px;
     height: ${size}px;
     border-radius: 50%;
     transform: translateX(-50%) rotateY(${getRotationRad(phase)}rad);
   `}
-`
+`;
 
 const Circle = styled.div`
   ${({ size }) => css`
@@ -50,12 +52,12 @@ const Circle = styled.div`
   `}
   border-radius: 50%;
   background: white;
-`
+`;
 
-const MoonCircle = ({ size, moon, ...props }) => {
-  const { phase } = moon
+const MoonCircle = ({ size, moon, rotation, ...props }) => {
+  const { phase } = moon;
   return (
-    <Wrapper size={size} {...props}>
+    <Wrapper rotation={rotation} size={size} {...props}>
       <Half size={size} isVisible={phase <= 0.5}>
         <Circle size={size} />
       </Half>
@@ -64,16 +66,18 @@ const MoonCircle = ({ size, moon, ...props }) => {
         <Circle size={size} />
       </Half>
     </Wrapper>
-  )
-}
+  );
+};
 
 MoonCircle.propTypes = {
   size: PropTypes.number,
   moon: PropTypes.object.isRequired,
-}
+  rotation: PropTypes.number,
+};
 
 MoonCircle.defaultProps = {
   size: 80,
-}
+  rotation: 0,
+};
 
-export default MoonCircle
+export default MoonCircle;
