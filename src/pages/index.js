3ecf2ce31a3ff600c7d5f9b3fee8c30464e0629b
Mateus Dal Bianco <mateus@dalbinaco.com>
2019-04-07 18:50:33 -0300

    Move new Date() to inside component function

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,16 @@ import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import Container from '../components/Container';
 
-const date = new Date();
-
-const IndexPage = () => (
-  <Layout>
-    <SEO keywords={[`ekadasi`, `ekadashi`, `calendar`, `moon`, `fasting`]} />
-    <Container maxWidth={640}>
-      <MoonInfo date={date} />
-    </Container>
-  </Layout>
-);
+const IndexPage = () => {
+  const date = new Date();
+  return (
+    <Layout>
+      <SEO keywords={[`ekadasi`, `ekadashi`, `calendar`, `moon`, `fasting`]} />
+      <Container maxWidth={640}>
+        <MoonInfo date={date} />
+      </Container>
+    </Layout>
+  );
+};
 
 export default IndexPage;
